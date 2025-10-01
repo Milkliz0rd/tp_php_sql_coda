4c428deea01cd9ce8fb7d0ab63f141bb5e661777
Milkliz0rd <morozoff_alexandre@outlook.com>
2025-10-01 23:14:32 +0200

    index page has been modify

--- src/partials/index.php
+++ src/partials/index.php
@@ -5,11 +5,16 @@ require __DIR__ . '/header.php'; ?>
 <!-- Landing Page -->
     <div class="flex flex-col bg-white gap-8 m-auto mt-20 border p-10 rounded-lg shadow-lg max-w-4xl">
       <h1 class="text-4xl font-bold underline text-center m-auto">Bienvenue sur mon exercice d'apprentissage de PHP et SQL</h1>
+      <?php if (empty($_SESSION['user'])): ?>
+      <p class="text-xl text-center m-auto">Vous n'êtes pas connecté. <a class="underline" href="./login.php">Connectez-vous</a> pour accéder aux fonctionnalités.</p>
+      <?php endif; ?>
+      <?php if (!empty($_SESSION['user'])): ?>
       <p class="text-xl text-center m-auto">Dans ce projet j'ai généré un tableau SQL "users".
         <br>
         J'ai ensuite créé une interface en PHP pour afficher les utilisateurs, en créer de nouveaux, les modifier et les supprimer.
         <br>
         <strong>Je vous invite à naviguer dans les différentes pages pour découvrir les fonctionnalités.</strong>
+        <?php endif; ?>
       </p>
     </div>
   </body>
